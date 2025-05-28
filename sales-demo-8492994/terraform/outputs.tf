2a03ba5a3a80adfb463aee84d0a853e8a881c20f
stackgen-automation <automation@stackgen.com>
2025-05-28 17:27:38 +0000

    Automated commit

---
+++ sales-demo-8492994/terraform/outputs.tf
@@ -0,0 +1,20 @@
+output "aws_s3_stackgen_5d42e787-7921-40e1-8c8d-b044556863f8_arn" {
+  value     = module.stackgen_5d42e787-7921-40e1-8c8d-b044556863f8.arn
+  sensitive = false
+}
+
+output "aws_s3_stackgen_5d42e787-7921-40e1-8c8d-b044556863f8_bucket_name" {
+  value     = module.stackgen_5d42e787-7921-40e1-8c8d-b044556863f8.bucket_name
+  sensitive = false
+}
+
+output "aws_s3_stackgen_5d42e787-7921-40e1-8c8d-b044556863f8_bucket_website_endpoint" {
+  value     = module.stackgen_5d42e787-7921-40e1-8c8d-b044556863f8.bucket_website_endpoint
+  sensitive = false
+}
+
+output "aws_s3_stackgen_5d42e787-7921-40e1-8c8d-b044556863f8_kms_arn" {
+  value     = module.stackgen_5d42e787-7921-40e1-8c8d-b044556863f8.kms_arn
+  sensitive = false
+}
+
